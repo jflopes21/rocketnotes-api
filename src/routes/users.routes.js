de731c81fafb4bba913a622fc598cc0c03ab4c04
jflopes21 <joaofrancisco21@yahoo.com.br>
2024-02-23 14:46:37 -0300

    Add: Updated function

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,5 +6,6 @@ const usersController = new UsersController();
 const userRoutes = Router();
 
 userRoutes.post("/", usersController.create);
+userRoutes.put("/:id", usersController.update);
 
 module.exports = userRoutes;
